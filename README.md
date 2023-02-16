3b27ed6b27c4d9c7d07c2fd3a3052a93d637f601
kyleroff112 <kyleroff112@gmail.com>
2023-02-16 09:04:19 -0500

    added functionality and changes to readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# weather-watcher
-Behold! A simple local forecast for the weary traveler.
+# Weather Watcher
+
+This is a simple weather app that allows you to search for the current weather and a 5-day forecast for any city. The app uses the OpenWeather API to get the weather data.
+
+To use the app, simply enter the name of a city in the search box and click the "Submit" button. The app will display the current weather for the city, including an icon, description, temperature, wind speed, and humidity. You can also click the "5-Day Forecast" button to see the forecast for the next 5 days.
+
+![screenshot of weather watcher app](./docs/assets/Screenshot%202023-02-16%20090109.png)
--- docs/assets/script.js
+++ docs/assets/script.js
@@ -1,3 +1,8 @@
+// https://api.openweathermap.org/geo/1.0/direct?q=hackettstown&limit=1&appid=2f41a48f60d6d854be758ad4552fc631
+// https://api.openweathermap.org/data/2.5/forecast?lat=40.8538944&lon=-74.8291638&units=imperial&appid=2f41a48f60d6d854be758ad4552fc631
+
+
+
 document.getElementById("searchButton").addEventListener("click", function () {
     event.preventDefault();
     var city = document.getElementById("city").value;
@@ -20,47 +25,60 @@ document.getElementById("searchButton").addEventListener("click", function () {
                         return response.json();
                     })
                     .then(function (data) {
-                        var dailyData = {};
-                        data.list.forEach(function (element) {
-                            var date = new Date(element.dt * 1000).toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: 'numeric' });
-                            if (!dailyData[date]) {
-                                dailyData[date] = {
-                                    temps: [],
-                                    humidities: [],
-                                    winds: [],
-                                    icons: []
-                                };
-                            }
-                            dailyData[date].temps.push(element.main.temp);
-                            dailyData[date].humidities.push(element.main.humidity);
-                            dailyData[date].winds.push(element.wind.speed);
-                            dailyData[date].icons.push(element.weather[0].icon);
-                        });
-                        for (var date in dailyData) {
-                            var temp = Math.round(dailyData[date].temps.reduce((a, b) => a + b) / dailyData[date].temps.length);
-                            var humidity = Math.round(dailyData[date].humidities.reduce((a, b) => a + b) / dailyData[date].humidities.length);
-                            var wind = Math.round(dailyData[date].winds.reduce((a, b) => a + b) / dailyData[date].winds.length);
-                            var icon = dailyData[date].icons[0];
-                            var iconURL = 'http://openweathermap.org/img/w/' + icon + '.png';
+                        document.getElementById("currentWeather").style.display = "block";
+                        var temp = data.list[0].main.temp;
+                        var tempMin = data.list[0].main.temp_min;
+                        var tempMax = data.list[0].main.temp_max;
+                        var humidity = data.list[0].main.humidity;
+                        var windSpeed = data.list[0].wind.speed;
+                        var weatherDescription = data.list[0].weather[0].description;
+                        var weatherIcon = data.list[0].weather[0].icon;
+                        var weatherIconURL = 'http://openweathermap.org/img/w/' + weatherIcon + '.png';
+                        console.log(weatherIconURL);
+                        document.getElementById("currentWeatherIcon").setAttribute("src", weatherIconURL);
+                        document.getElementById("currentWeatherIcon").setAttribute("alt", weatherDescription);
+                        document.getElementById("currentWeatherDescription").textContent = weatherDescription;
+                        document.getElementById("currentWeatherTemp").textContent = temp + " °F";
+                        document.getElementById("currentTempMin").textContent = "Min: " + tempMin + " °F";
+                        document.getElementById("currentTempMax").textContent = "Max: " + tempMax + " °F";
+                        document.getElementById("currentWeatherWind").textContent = "Wind: " + windSpeed + " mph";
+                        document.getElementById("currentWeatherHumidity").textContent = "Humidity: " + humidity + " %";
+                        var forecast = data.list;
+                        // Get the container element to append the forecast elements to
+                        var forecastContainer = document.getElementById("forecast");
+                        // Clear any existing forecast elements
+                        document.getElementById("forecast").style.display = "block";
 
-                            var card = document.createElement("div");
-                            card.classList.add("card");
-                            var cardBody = document.createElement("div");
-                            cardBody.classList.add("card-body");
-                            var cardTitle = document.createElement("h5");
-                            cardTitle.classList.add("card-title");
-                            cardTitle.innerHTML = date;
-                            var cardText = document.createElement("p");
-                            cardText.classList.add("card-text");
-                            cardText.innerHTML = "Temp: " + temp + " &#8457; Humidity: " + humidity + " Wind: " + wind + " MPH";
-                            var cardImg = document.createElement("img");
-                            cardImg.setAttribute("src", iconURL);
-                            cardImg.setAttribute("alt", "weather icon");
-                            cardBody.appendChild(cardTitle);
-                            cardBody.appendChild(cardText);
-                            cardBody.appendChild(cardImg);
-                            card.appendChild(cardBody);
-                            document.getElementById("forecast").appendChild(card);
+                        // Loop through each forecast and create HTML elements for each one
+                        for (var i = 0; i < forecast.length; i++) {
+                            if (forecast[i].dt_txt.includes("18:00:00")) {
+                                var forecastTemp = forecast[i].main.temp;
+                                var forecastTempMin = forecast[i].main.temp_min;
+                                var forecastTempMax = forecast[i].main.temp_max;
+                                var forecastHumidity = forecast[i].main.humidity;
+                                var forecastWindSpeed = forecast[i].wind.speed;
+                                var forecastWeatherDescription = forecast[i].weather[0].description;
+                                var forecastWeatherIcon = forecast[i].weather[0].icon;
+                                var forecastWeatherIconURL = 'http://openweathermap.org/img/w/' + forecastWeatherIcon + '.png';
+                                var date = forecast[i].dt_txt.substring(0, 10);
+
+                                // Create a new forecast element
+                                var forecastElement = document.createElement("div");
+                                forecastElement.classList.add("forecast");
+
+                                // Add the forecast content to the element
+                                forecastElement.innerHTML = '<h2>' + date + '</h2>' +
+                                    '<img class="col" src="' + forecastWeatherIconURL + '" alt="' + forecastWeatherDescription + '">' +
+                                    '<div class="col">' + forecastWeatherDescription + '</div>' +
+                                    '<div class="col">' + forecastTemp + ' °F</div>' +
+                                    '<div class="col">Min: ' + forecastTempMin + ' °F</div>' +
+                                    '<div class="col">Max: ' + forecastTempMax + ' °F</div>' +
+                                    '<div class="col">Wind: ' + forecastWindSpeed + ' mph</div>' +
+                                    '<div class="col">Humidity: ' + forecastHumidity + '%</div>';
+
+                                // Append the forecast element to the container
+                                forecastContainer.appendChild(forecastElement);
+                            }
                         }
                     })
             })
